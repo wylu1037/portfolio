bda4aef999dace37bc9eac84afceca15875a4f2a
wylu1037 <wylu1037@gmail.com>
2024-11-05 23:11:09 +0800

    feat: prettier-plugin-tailwindcss integration

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,13 +3,13 @@ import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-w
 const Hero = () => {
   return (
     <BackgroundBeamsWithCollision>
-      <h2 className="text-2xl max-w-screen-lg relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
+      <h2 className="relative z-20 max-w-screen-lg text-center font-sans text-2xl font-bold tracking-tight text-black dark:text-white md:text-4xl lg:text-7xl">
         What&apos;s cooler than Beams?{" "}
         <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
-          <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+          <div className="absolute left-0 top-[1px] bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 bg-clip-text bg-no-repeat py-4 text-transparent [text-shadow:0_0_rgba(0,0,0,0.1)]">
             <span className="">Exploding beams.</span>
           </div>
-          <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
+          <div className="relative bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 bg-clip-text bg-no-repeat py-4 text-transparent">
             <span className="">Exploding beams.</span>
           </div>
         </div>
