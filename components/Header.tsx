d6d3f2732a7bb8a7287ec5f789a70d572ee87c31
wylu1037 <wylu1037@gmail.com>
2024-11-06 23:55:21 +0800

    feat: add Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,87 +1,73 @@
 "use client";
 
 import React from "react";
-import { FloatingDock } from "@/components/ui/floating-dock";
-import {
-  IconBrandGithub,
-  IconBrandX,
-  IconExchange,
-  IconHome,
-  IconNewSection,
-  IconTerminal2,
-} from "@tabler/icons-react";
-import Image from "next/image";
+import { IconChevronsRight, IconGripVertical } from "@tabler/icons-react";
 
 interface HeaderProps {
   title: string;
 }
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
-  const links = [
-    {
-      title: "Home",
-      icon: (
-        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-
-    {
-      title: "Products",
-      icon: (
-        <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-    {
-      title: "Components",
-      icon: (
-        <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-    {
-      title: "Aceternity UI",
-      icon: (
-        <Image
-          src="https://assets.aceternity.com/logo-dark.png"
-          width={20}
-          height={20}
-          alt="Aceternity Logo"
-        />
-      ),
-      href: "#",
-    },
-    {
-      title: "Changelog",
-      icon: (
-        <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-
-    {
-      title: "Twitter",
-      icon: (
-        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-    {
-      title: "GitHub",
-      icon: (
-        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-  ];
   return (
-    <div className="flex items-center justify-center h-[35rem] w-full">
-      <FloatingDock
-        mobileClassName="translate-y-20" // only for demo, remove for production
-        items={links}
-      />
-    </div>
+    <header className="relative z-[999]">
+      <nav className="fixed left-1/2 top-4 flex -translate-x-1/2 items-center justify-between gap-4 rounded-full border-2 border-gray-100 px-6 py-1 shadow-lg backdrop-blur-xl">
+        {/* Home */}
+        <div className="flex items-center gap-2">
+          <IconGripVertical className="h-5 w-5 cursor-grab text-gray-300 hover:text-gray-500" />
+          <a
+            href="#"
+            className="rounded-md px-2 py-1 font-semibold text-stone-600 hover:bg-stone-200"
+          >
+            Home
+          </a>
+        </div>
+        {/* About */}
+        <a
+          href="#"
+          className="rounded-md px-2 py-1 font-semibold text-stone-600 hover:bg-stone-200"
+        >
+          About
+        </a>
+        {/* Projects */}
+        <a
+          href="#"
+          className="flex rounded-md px-2 py-1 text-lg font-bold text-gray-900"
+        >
+          <p className="motion-preset-oscillate motion-duration-[2s]">P</p>
+          <p className="motion-preset-oscillate motion-duration-[2s] motion-delay-75">
+            r
+          </p>
+          <p className="motion-preset-oscillate motion-duration-[2s] motion-delay-100">
+            o
+          </p>
+          <p className="motion-preset-oscillate motion-duration-[2s]">j</p>
+          <p className="motion-preset-oscillate motion-duration-[2s] motion-delay-75">
+            e
+          </p>
+          <p className="motion-preset-oscillate motion-duration-[2s] motion-delay-150">
+            c
+          </p>
+          <p className="motion-preset-oscillate motion-duration-[2s]">t</p>
+          <p className="motion-preset-oscillate motion-duration-[2s] motion-delay-75">
+            s
+          </p>
+        </a>
+        {/* Blog */}
+        <a
+          href="#"
+          className="rounded-md px-2 py-1 font-semibold text-stone-600 hover:bg-stone-200"
+        >
+          Blog
+        </a>
+        {/* Contact */}
+        <a
+          href="#"
+          className="flex items-center gap-1 rounded-md bg-yellow-200 px-4 py-1 font-semibold text-stone-700 hover:bg-gray-100"
+        >
+          Contact <IconChevronsRight className="h-4 w-4" />
+        </a>
+      </nav>
+    </header>
   );
 };
 
