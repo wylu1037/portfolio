f2d8abdec0db43085a686efdcd904955a6f1e7a6
wylu1037 <wylu1037@gmail.com>
2024-11-06 09:01:30 +0800

    feat: tailwindcss-motion

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,11 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <div>
+      <div className="mt-20 flex justify-center">
+        <div className="motion-preset-confetti flex size-6 items-center justify-center">
+          🎉
+        </div>
+      </div>
       <Header title="Home" />
       <Hero />
     </div>
