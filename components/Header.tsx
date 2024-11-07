dc23c24b6d60c763d85102536a5684fd766c122e
wylu1037 <wylu1037@gmail.com>
2024-11-07 08:04:54 +0800

    chore(Header): adjust text color

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,14 +10,14 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
-    <header className="relative z-[999]">
+    <header className="relative z-50">
       <nav className="fixed left-1/2 top-4 flex -translate-x-1/2 items-center justify-between gap-4 rounded-full border-2 border-gray-100 px-6 py-1 shadow-lg backdrop-blur-xl">
         {/* Home */}
         <div className="flex items-center gap-2">
           <IconGripVertical className="h-5 w-5 cursor-grab text-gray-300 hover:text-gray-500" />
           <Link
             href="/"
-            className="rounded-md px-2 py-1 font-semibold text-stone-600 hover:bg-stone-200"
+            className="rounded-md px-2 py-1 font-semibold text-stone-500 hover:bg-stone-200"
           >
             Home
           </Link>
@@ -25,14 +25,14 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
         {/* About */}
         <Link
           href="/about"
-          className="rounded-md px-2 py-1 font-semibold text-stone-600 hover:bg-stone-200"
+          className="rounded-md px-2 py-1 font-semibold text-stone-500 hover:bg-stone-200"
         >
           About
         </Link>
         {/* Projects */}
         <Link
           href="/projects"
-          className="flex rounded-md px-2 py-1 text-lg font-bold text-gray-900"
+          className="flex rounded-md px-2 py-1 text-lg font-bold text-stone-500 hover:bg-stone-200"
         >
           <p className="motion-preset-oscillate motion-duration-[2s]">P</p>
           <p className="motion-preset-oscillate motion-duration-[2s] motion-delay-75">
@@ -56,7 +56,7 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
         {/* Blog */}
         <Link
           href="/blog"
-          className="rounded-md px-2 py-1 font-semibold text-stone-600 hover:bg-stone-200"
+          className="rounded-md px-2 py-1 font-semibold text-stone-500 hover:bg-stone-200"
         >
           Blog
         </Link>
