9f216131c7d686213577e9052a13a560e70f5af5
wylu1037 <wylu1037@gmail.com>
2024-11-10 22:55:40 +0800

    feat: use number-flow

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,13 +1,42 @@
-import React from "react";
-import SectionTitle from "./shared/SectionTitle";
+"use client";
+
+import React, { useState } from "react";
 import Container from "./shared/Container";
 import Wrapper from "./shared/Wrapper";
+import NumberFlow from "@number-flow/react";
+import { Button } from "./ui/button";
 
 const Experience = () => {
+  const [seconds, setSeconds] = useState<number>(0);
+  const minutes = Math.floor(seconds / 60);
+  const displatSeconds = seconds % 60;
+
   return (
     <Wrapper className="mt-28">
       <Container>
-        <SectionTitle>Experiences</SectionTitle>
+        <div className="flex items-center justify-center gap-4">
+          <div>
+            <NumberFlow
+              value={minutes}
+              className="text-4xl font-semibold"
+              format={{ notation: "compact", minimumIntegerDigits: 2 }}
+              locales={"en-US"}
+            />
+            <span className="text-4xl font-semibold">:</span>
+            <NumberFlow
+              value={displatSeconds}
+              className="text-4xl font-semibold"
+              format={{ notation: "compact", minimumIntegerDigits: 2 }}
+              locales={"en-US"}
+            />
+          </div>
+          <Button onClick={() => setSeconds(seconds + 1)} variant="default">
+            Increse
+          </Button>
+          <Button onClick={() => setSeconds(seconds - 1)} variant="outline">
+            Decrese
+          </Button>
+        </div>
       </Container>
     </Wrapper>
   );
