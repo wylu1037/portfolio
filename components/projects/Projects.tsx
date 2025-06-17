d597e8cd53c504d39526b19ac040cd5f0d76d3a7
wylu1037 <wylu1037@gmail.com>
2025-06-17 09:38:44 +0800

    feat: projects

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -1,11 +1,56 @@
 import Wrapper from "@/components/shared/Wrapper";
 import Container from "@/components/shared/Container";
+import { ProjectCard } from "./ProjectCard";
+import { DATA } from "@/data/projects";
+import { BlurFade } from "@/components/magicui/blur-fade";
+
+const BLUR_FADE_DELAY = 0.04;
 
 export default function Projects() {
   return (
     <Wrapper className="mt-28 bg-transparent">
       <Container>
-        <div>Projects</div>
+        <section id="projects">
+          <div className="w-full space-y-12 py-12">
+            <BlurFade delay={BLUR_FADE_DELAY * 11}>
+              <div className="flex flex-col items-center justify-center space-y-4 text-center">
+                <div className="space-y-2">
+                  <div className="bg-foreground text-background inline-block rounded-lg px-3 py-1 text-sm">
+                    My Projects
+                  </div>
+                  <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                    Check out my latest work
+                  </h2>
+                  <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                    I&apos;ve worked on a variety of projects, from simple
+                    websites to complex web applications. Here are a few of my
+                    favorites.
+                  </p>
+                </div>
+              </div>
+            </BlurFade>
+            <div className="mx-auto grid max-w-[800px] grid-cols-1 gap-3 sm:grid-cols-2">
+              {DATA.projects.map((project, id) => (
+                <BlurFade
+                  key={project.title}
+                  delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+                >
+                  <ProjectCard
+                    href={project.href}
+                    key={project.title}
+                    title={project.title}
+                    description={project.description}
+                    dates={project.dates}
+                    tags={project.technologies}
+                    image={project.image}
+                    video={project.video}
+                    links={project.links}
+                  />
+                </BlurFade>
+              ))}
+            </div>
+          </div>
+        </section>
       </Container>
     </Wrapper>
   );
